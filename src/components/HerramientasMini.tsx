1ec7c0efb56eb29c01e47cbbafdce6afaa911112
Letalandroid <carlossoncra@gmail.com>
2024-03-01 09:39:36 -0500

    ⛔ Quit cursor-normal prop on HerramientasMini

--- src/components/HerramientasMini.tsx
+++ src/components/HerramientasMini.tsx
@@ -6,7 +6,7 @@ const HerramientasCardMini = ({
 	description,
 }: HerramientasCardMiniI) => {
 	return (
-		<div className="w-80 flex flex-col items-center text-center text-white gap-2 cursor-default transition hover:scale-110">
+		<div className="w-80 flex flex-col items-center text-center text-white gap-2 transition hover:scale-110">
 			<img src={icon} alt="Icono de la herramienta" />
 			<h3 className="font-bold text-2xl">{title}</h3>
 			<p className="text-[#BDC1CA]">{description}</p>
