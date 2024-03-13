d83c0c404ba5df2ea56e7ac40af38ee112856265
Letalandroid <carlossoncra@gmail.com>
2024-03-13 18:59:21 -0500

    Finish graphics (1)

--- src/pages/GetGraphics.tsx
+++ src/pages/GetGraphics.tsx
@@ -8,47 +8,101 @@ import {
 	Tooltip,
 	XAxis,
 	YAxis,
-    Brush
+	Brush,
 } from 'recharts';
 import {
 	collection,
 	getDocs,
 	DocumentData,
-    query,
-    where
+	query,
+	where,
 } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
-import { toast, ToastContainer } from 'react-toastify'
+import { toast, ToastContainer } from 'react-toastify';
 
 const GetGraphics = () => {
 	const [type, setType] = useState('favFeature');
 	const [data, setData] = useState<DocumentData[] | undefined>(undefined);
-    const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(false);
+
+	const favFeatureStats = [
+		{ Crowdmetwer: 0 },
+		{ 'Rutinas Personalizadas': 0 },
+		{ 'Reportes de progreso': 0 },
+		{ 'Ver el progreso de mis amigos': 0 },
+	];
+
+	const soStats = [
+		{ iOS: 0 },
+		{ Android: 0 },
+		{ Otro: 0 }
+	];
+
+	const motivationStats = [
+		{ Salud: 0 },
+		{ Estetica: 0 },
+		{ 'Mi gym crush': 0 },
+		{ Fuerza: 0 },
+	];
+
+	const rateRutinaStats = [
+		{ 'Podria mejorar': 0 },
+		{ 'Hago la que me dieron en el gym': 0 },
+		{ 'No tengo rutina': 0 },
+		{ 'No me gusta': 0 },
+	];
 
 	const getData = async () => {
-        setLoading(true);
-        const usersRef = collection(db, 'users-beta');
-        const q = query(usersRef, where('favFeature', '!=', false));
-        const capitalNotFalseRes = await getDocs(q);
-        const newData = capitalNotFalseRes.docs.map((doc) => doc.data());
-        setData(newData);
-        console.log(data)
-        setLoading(false);
+		setLoading(true);
+		const usersRef = collection(db, 'users-beta');
+		const q = query(usersRef, where('favFeature', '!=', false));
+		const capitalNotFalseRes = await getDocs(q);
+		const newData = capitalNotFalseRes.docs.map((doc) => doc.data());
+
+		newData.forEach((prop) => {
+			if (type === 'favFeature') {
+				const feature = prop.favFeature;
+				const index = favFeatureStats.findIndex((item) => feature in item);
+				if (index !== -1) {
+					favFeatureStats[index][feature]++;
+				}
+			} else if (type === 'so') {
+				const so = prop.so;
+				const index = soStats.findIndex((item) => so in item);
+				if (index !== -1) {
+					soStats[index][so]++;
+				}
+			} else if (type === 'motivation') {
+				const motivation = prop.motivation;
+				const index = motivationStats.findIndex((item) => motivation in item);
+				if (index !== -1) {
+					motivationStats[index][motivation]++;
+				}
+			} else if (type === 'rateRutina') {
+				const rateRutina = prop.rateRutina;
+				const index = rateRutinaStats.findIndex((item) => rateRutina in item);
+				if (index !== -1) {
+					rateRutinaStats[index][rateRutina]++;
+				}
+			}
+		});
+
+		if (type === 'favFeature') {
+			setData(favFeatureStats);
+		} else if (type === 'so') {
+			setData(soStats);
+		} else if (type === 'motivation') {
+			setData(motivationStats);
+		} else if (type === 'rateRutina') {
+			setData(rateRutinaStats);
+		}
+
+		setLoading(false);
 	};
 
 	useEffect(() => {
 		getData();
 	}, [type]);
-
-    const data_example = [
-			{ name: 'María', age: 10, weight: 60 },
-			{ name: 'Karina', age: 25, weight: 70 },
-			{ name: 'Susana', age: 15, weight: 65 },
-			{ name: 'Pedro', age: 35, weight: 85 },
-			{ name: 'Felipe', age: 12, weight: 48 },
-			{ name: 'Laura', age: 30, weight: 69 },
-			{ name: 'Adrián', age: 15, weight: 78 },
-		];
 
 	return (
 		<div className="h-screen flex justify-center items-center">
@@ -87,12 +141,49 @@ const GetGraphics = () => {
 								bottom: 5,
 							}}>
 							<CartesianGrid strokeDasharray="4 1 2" />
-							<XAxis />
-							<YAxis dataKey={type} />
+							<XAxis dataKey='simple_datakey' />
+							<YAxis />
 							<Tooltip />
 							<Legend />
-							<Brush dataKey={type} height={30} stroke="#8884d8" />
-							<Bar dataKey={type} fill="#6b48ff" />
+							{type === 'favFeature' ? (
+								<>
+									<Bar dataKey="Crowdmetwer" fill="#f00" />
+									<Bar dataKey="Rutinas Personalizadas" fill="#0a0" />
+									<Bar dataKey="Reportes de progreso" fill="#00f" />
+									<Bar dataKey="Ver el progreso de mis amigos" fill="#f0f" />
+								</>
+							) : (
+								''
+							)}
+							{type === 'motivation' ? (
+								<>
+									<Bar dataKey="Salud" fill="#0a0" />
+									<Bar dataKey="Estetica" fill="#00f" />
+									<Bar dataKey="Mi gym crush" fill="#f0f" />
+									<Bar dataKey="Fuerza" fill="#a00" />
+								</>
+							) : (
+								''
+							)}
+							{type === 'so' ? (
+								<>
+									<Bar dataKey="Android" fill="#0a0" />
+									<Bar dataKey="iOS" fill="#00f" />
+									<Bar dataKey="Otro" fill="#000" />
+								</>
+							) : (
+								''
+							)}
+							{type === 'rateRutina' ? (
+								<>
+									<Bar dataKey="Podria mejorar" fill="#0a0" />
+									<Bar dataKey="Hago la que me dieron en el gym" fill="#f00" />
+									<Bar dataKey="No tengo rutina" fill="#00f" />
+									<Bar dataKey="No me gusta" fill="#000" />
+								</>
+							) : (
+								''
+							)}
 						</BarChart>
 					</ResponsiveContainer>
 				)}
