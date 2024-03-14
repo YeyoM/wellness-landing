4de585dce953a1024ec9e858b9fe30336d2c54b6
Letalandroid <carlossoncra@gmail.com>
2024-03-14 08:33:47 -0500

    Add types into vite-env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -22,3 +22,29 @@ export interface BtnRptaI {
 	value: string;
 	set: React.Dispatch<React.SetStateAction<string>>;
 }
+export interface FavFeatureStatsI {
+	Crowdmetwer: number;
+	'Rutinas Personalizadas': number;
+	'Reportes de progreso': number;
+	'Ver el progreso de mis amigos': number;
+}
+
+export interface SoStatsI {
+	iOS: number;
+	Android: number;
+	Otro: number;
+}
+
+export interface MotivationStatsI {
+	Salud: number;
+	Estetica: number;
+	'Mi gym crush': number;
+	Fuerza: number;
+}
+
+export interface RateRutinaStatsI {
+	'Podria mejorar': number;
+	'Hago la que me dieron en el gym': number;
+	'No tengo rutina': number;
+	'No me gusta': number;
+}
