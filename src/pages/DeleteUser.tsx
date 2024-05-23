5082806aa9c617a620aadf6d1b5329d816565195
Letalandroid <carlossoncra@gmail.com>
2024-05-23 10:12:48 -0500

    Usuario eliminado exitosamente

--- src/pages/DeleteUser.tsx
+++ src/pages/DeleteUser.tsx
@@ -11,6 +11,7 @@ import {
 const DeleteUser = () => {
 	const [confirm, setConfirm] = useState(false);
 	const [userEmail, setUserEmail] = useState('');
+    const [userDelete, setUserDelete] = useState(false);
 
 	const returnHome = () => {
 		location.href = '/';
@@ -28,7 +29,7 @@ const DeleteUser = () => {
 		if (!querySnapshot.empty) {
 			const userDoc = querySnapshot.docs[0];
 			await deleteDoc(userDoc.ref);
-			console.log('Usuario eliminado exitosamente');
+            setUserDelete(true);
 		} else {
 			console.log(
 				'No se encontró ningún usuario con el correo electrónico proporcionado'
@@ -59,21 +60,35 @@ const DeleteUser = () => {
 						</button>
 					</div>
 				</section>
-				<section className={`${confirm ? '' : 'hidden'} flex flex-col gap-2`}>
-					<p className="text-2xl">Escriba su correo electrónico</p>
-					<div className="flex flex-col justify-around gap-4">
-						<input
-							onChange={(e) => setUserEmail(e.target.value)}
-							className="w-[30em] p-3"
-							type="text"
-							placeholder={'example@example.example'}
-						/>
-						<button
-							onClick={removeUser}
-							className="font-bold bg-text_blue text-white text-lg px-14 py-2 rounded-lg">
-							Confirmar
-						</button>
-					</div>
+				{userDelete ? (
+					''
+				) : (
+					<section className={`${confirm ? '' : 'hidden'} flex flex-col gap-2`}>
+						<p className="text-2xl">Escriba su correo electrónico</p>
+						<div className="flex flex-col justify-around gap-4">
+							<input
+								onChange={(e) => setUserEmail(e.target.value)}
+								className="w-[30em] p-3"
+								type="text"
+								placeholder={'example@example.example'}
+							/>
+							<button
+								onClick={removeUser}
+								className="font-bold bg-text_blue text-white text-lg px-14 py-2 rounded-lg">
+								Confirmar
+							</button>
+						</div>
+					</section>
+				)}
+				<section
+					className={`${userDelete ? '' : 'hidden'} flex flex-col items-center gap-3`}>
+					<p className="text-white text-2xl font-bold">Usuario eliminado exitosamente</p>
+
+					<button
+						onClick={returnHome}
+						className="font-bold bg-text_blue text-white text-lg px-10 py-2 rounded-lg">
+						Volver
+					</button>
 				</section>
 			</main>
 		</>
